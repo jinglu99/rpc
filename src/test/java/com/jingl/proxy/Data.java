5ef1746e312791804f3ad9fb4a8476f8843fa82b
汪京陆 <wangjinglu@souche.com>
2018-02-10 14:59:10 -0500

    0.1

--- src/test/java/com/jingl/proxy/Data.java
+++ src/test/java/com/jingl/proxy/Data.java
@@ -1,6 +1,7 @@
 package com.jingl.proxy;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 /**
  * Created by Ben on 13/12/2017.
@@ -12,4 +13,61 @@ public class Data implements Serializable {
     float f = 1.0f;
     short s = 0;
     byte b = 1;
+    BigDecimal decimal = new BigDecimal(1.0);
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public void setD(double d) {
+        this.d = d;
+    }
+
+    public float getF() {
+        return f;
+    }
+
+    public void setF(float f) {
+        this.f = f;
+    }
+
+    public short getS() {
+        return s;
+    }
+
+    public void setS(short s) {
+        this.s = s;
+    }
+
+    public byte getB() {
+        return b;
+    }
+
+    public void setB(byte b) {
+        this.b = b;
+    }
+
+    public BigDecimal getDecimal() {
+        return decimal;
+    }
+
+    public void setDecimal(BigDecimal decimal) {
+        this.decimal = decimal;
+    }
 }
